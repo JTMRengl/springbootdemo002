ade156803225c954f13001de5812126b191f7948
小蹲蹲 <12428668@qq.com>
2019-12-14 21:51:59 +0800

    Initial commit

---
+++ src/main/java/com/offcn/po/User.java
@@ -0,0 +1,26 @@
+package com.offcn.po;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name", nullable = true, length = 200)
+    private String name;
+
+    @Column(name = "age", nullable = true, length = 4)
+    private Integer age;
+
+}
